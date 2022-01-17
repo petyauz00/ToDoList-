86dec0b9179f7364f63f127cde55c4cc5189ebbf
petyauz00 <85995407+petyauz00@users.noreply.github.com>
2022-01-17 19:48:39 +0200

    Add files via upload

---
+++ date.js
@@ -0,0 +1,18 @@
+// find the date and chenge the format
+ exports.getDate = function(){
+  const today = new Date();
+  const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long'
+  };
+  return today.toLocaleDateString("en-US", options);
+}
+
+exports.getDay = function(){
+  const today  = new Date();
+  const options = {
+    weekday:"long"
+  };
+  return today.toLocaleDateString("en-US", options);
+}
